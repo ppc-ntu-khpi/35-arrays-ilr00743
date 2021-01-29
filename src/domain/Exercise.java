6c647e55560d0a46f8f841c992e7acf3ecbcf200
Illia_ryzhiy <ilr00743@gmail.com>
2021-01-29 18:37:41 +0200

    File edited

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,20 @@
 package domain;
-
+/**
+ * Class that represent an exercise
+ * @author Illia_R
+ */
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    
+    /**
+     * Method that fills the array
+     * @param array integer array
+     */
+    public static void fillArray(int[] array){
+        int n = (int) Math.ceil(array.length / 2.0) - 1;
+        
+        for (int i = 0; i < n; i++) {
+            array[i] = n - i;
+            array[array.length - i - 1] = array[i];
+        }
     }
 }
